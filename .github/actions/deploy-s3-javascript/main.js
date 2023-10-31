7c55413d30b224cd8d8322a2b2038f7d060a515f
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2023-10-31 18:05:17 +0100

    javascript

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,15 @@
-function run()  {
-    const core = require('@actions/core');
-    const github = require('@actions/github');
-    const exec = require('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
+function run()  {
     core.notice('Hello from the custom JavaScript Action.')
+
+    const bucket = core.getInput('s3-name', { required: true});
+    const region = core.getInput('s3-region', { required: true});
+    const folder = core.getInput('s3-folder', { required: true});
+
+    exec.exec(`aws s3 sync ${folder} s3://${bucket} --region ${region}`)
 };
 
 run();
